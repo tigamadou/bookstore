0f2955610c2c7a2eba459bc6f7eb98a829d76ad6
genzaraky <amadou140589@gmail.com>
2021-04-15 18:02:06 +0100

    createBook action added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,14 @@
 const ADD_BOOK = 'ADD_BOOK';
+const generateId = () => Math.random().toString(36).substr(2, 5);
 
-export default { ADD_BOOK };
+const defaultBook = {
+  id: generateId(),
+  title: '',
+  category: '',
+};
+const createBook = () => ({
+  type: ADD_BOOK,
+  book: defaultBook,
+});
+
+export default { ADD_BOOK, createBook };
