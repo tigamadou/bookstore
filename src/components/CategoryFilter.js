49699f3aa80fa1b33b0ffb25474adaf755fd79a8
genzaraky <amadou140589@gmail.com>
2021-04-16 11:31:03 +0100

    handleFilterChange implemented in categoryFilter component

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -2,10 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const categories = ['All', ...['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi']];
-const CategoryFilter = ({ filter }) => (
+const CategoryFilter = ({ filter, handleFilterChange }) => (
   <select
     value={filter}
     name="filter"
+    onChange={handleFilterChange}
   >
     {categories.map((cat) => <option value={cat} key={cat}>{cat}</option>)}
   </select>
@@ -13,10 +14,12 @@ const CategoryFilter = ({ filter }) => (
 
 CategoryFilter.propTypes = {
   filter: PropTypes.string,
+  handleFilterChange: PropTypes.func,
 };
 
 CategoryFilter.defaultProps = {
   filter: 'All',
+  handleFilterChange: null,
 };
 
 export default CategoryFilter;
