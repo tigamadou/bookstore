894f4c16b9f58d375374a1fa6927493e6f0cb4c9
genzaraky <amadou140589@gmail.com>
2021-04-16 14:35:12 +0100

    book component structure edited. Book style added

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,12 +5,45 @@ const Book = (book) => {
     id, title, category, handleRemoveBook,
   } = book;
   return (
-    <tr>
-      <td>{id}</td>
-      <td>{title}</td>
-      <td>{category}</td>
-      <td><button type="button" onClick={() => handleRemoveBook({ id })}>Remove Book</button></td>
-    </tr>
+
+    <div className="book_card">
+      <div className="book_info">
+        <div className="card_left_content">
+          <div className="book_category">{category}</div>
+          <div className="book_title">{title}</div>
+          <div className="book_author">Author</div>
+        </div>
+        <div className="boo_footer">
+          <ul>
+            <li><a href="/#">Comments</a></li>
+            <li><a href="/#" role="button" onClick={() => handleRemoveBook({ id })}>Remove</a></li>
+            <li><a href="/#">Edit</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="book_details">
+        <div className="book_progress">
+          <div className="book_progress_chart">
+            chart
+          </div>
+          <div className="book_progress_details">
+            <div className="book_progress_value">64%</div>
+            <div className="book_progress_text">COMPLETED</div>
+          </div>
+        </div>
+        <div className="book_chapter">
+          <div className="title">
+            Current chapter
+          </div>
+          <div className="value">
+            Chapter 17
+          </div>
+          <div className="update">
+            <button type="button" className="update_progress">Update progress</button>
+          </div>
+        </div>
+      </div>
+    </div>
 
   );
 };
