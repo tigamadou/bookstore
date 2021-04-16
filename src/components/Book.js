3834ef272405ef47e9fd75af28d19b23f1bad1a4
genzaraky <amadou140589@gmail.com>
2021-04-16 10:44:30 +0100

    milestone2 and 3 merged

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const Book = (book) => {
-  const { id, title, category } = book;
-
+  const {
+    id, title, category, handleRemoveBook,
+  } = book;
   return (
     <tr>
       <td>{id}</td>
       <td>{title}</td>
       <td>{category}</td>
+      <td><button type="button" onClick={() => handleRemoveBook({ id })}>Remove</button></td>
     </tr>
   );
 };
