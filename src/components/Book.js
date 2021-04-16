bfb0992c210ae159b9734f160dcbcf6018ff0e6e
genzaraky <amadou140589@gmail.com>
2021-04-16 10:22:20 +0100

    book component edited

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,29 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const Book = ({
-  id, title, category, handleRemoveBook,
-}) => (
-  <tr>
-    <td>{id}</td>
-    <td>{title}</td>
-    <td>{category}</td>
-    <td><button type="button" onClick={() => handleRemoveBook({ id })}>Remove Book</button></td>
-  </tr>
-);
+const Book = (book) => {
+  const {
+    id, title, category, handleRemoveBook,
+  } = book;
+  return (
+    <tr>
+      <td>{id}</td>
+      <td>{title}</td>
+      <td>{category}</td>
+      <td><button type="button" onClick={() => handleRemoveBook({ id })}>Remove Book</button></td>
+    </tr>
 
-Book.propTypes = {
-  id: PropTypes.number,
-  title: PropTypes.string,
-  category: PropTypes.string,
-  handleRemoveBook: PropTypes.func,
-};
-
-Book.defaultProps = {
-  id: null,
-  title: '',
-  category: '',
-  handleRemoveBook: null,
+  );
 };
 
 export default Book;
