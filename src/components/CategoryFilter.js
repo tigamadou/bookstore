c821a63380443e8a4ead5c6768998499481fc957
genzaraky <amadou140589@gmail.com>
2021-04-21 14:23:59 +0100

    categoryFilter component refactored

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -3,26 +3,28 @@ import PropTypes from 'prop-types';
 
 const categories = ['All', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 const CategoryFilter = ({ filter, handleFilterChange }) => (
-  <div className="category_filter">
-    <span>Category</span>
-    <select
-      value={filter}
-      name="filter"
-      onChange={handleFilterChange}
-    >
-      {categories.map((cat) => <option value={cat} key={cat}>{cat}</option>)}
-    </select>
-  </div>
+  <>
+    <div className="filter">
+      <span>Category</span>
+      <select
+        value={filter}
+        name="filter"
+        onChange={handleFilterChange}
+      >
+        {/* eslint-disable-next-line max-len */}
+        <>{categories.map((category) => <option key={category} value={category}>{category}</option>)}</>
+      </select>
+    </div>
+  </>
 );
 
-CategoryFilter.propTypes = {
-  filter: PropTypes.string,
-  handleFilterChange: PropTypes.func,
-};
-
 CategoryFilter.defaultProps = {
-  filter: 'All',
   handleFilterChange: null,
+  filter: 'All',
+};
+CategoryFilter.propTypes = {
+  handleFilterChange: PropTypes.func,
+  filter: PropTypes.string,
 };
 
 export default CategoryFilter;
